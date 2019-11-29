21d5150c53dee8b4db1f81384a9f95e08376d22a
teslvova <teslvova@gmail.com>
2019-11-29 13:12:10 +0200

    test

--- src/test/java/org/selenide/Google/GoogleSearchFieldInMainPage.java
+++ src/test/java/org/selenide/Google/GoogleSearchFieldInMainPage.java
@@ -33,11 +33,3 @@ public class GoogleSearchFieldInMainPage {
    }
 
 
-
-
-//
-//    git init
-//    git add README.md
-//        git commit -m "first commit"
-//        git remote add origin https://github.com/Teslenko/Ready_Selenide-Autotest.git
-//        git push -u origin master
